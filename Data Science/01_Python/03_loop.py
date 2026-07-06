20bd0d0de6bee7a6847e97729fe4df3e6e47765e
Yug Patel <pyugpatel189@gmail.com>
2026-07-06 20:58:56 +0530

    Add Python basics and pattern practice

--- Data Science/01_Python/03_loop.py
+++ Data Science/01_Python/03_loop.py
@@ -147,10 +147,126 @@
 
 ## 16 Reverse No 
 
-user_input = int(input('Enter a No' ))
-a= 0
-while (user_input > 0 ):
-    user_input = user_input % 10
-    user_input/=10
-    user_input = () 
-    print(a)
+
+
+# For loop 
+
+# 1 1 to N 
+
+# user_input = int(input('Enter a No. '))
+
+# for i in range (user_input+1):
+#     print (i,end=",")
+
+# 2 n to 1
+
+# user_input = int(input('Enter a No. '))
+# for i in range (user_input,0,-1):
+#     print (i,end=",")
+
+# 3 n1 to n2
+
+# user_input = int(input('Enter a No. '))
+# user_input1 = int(input('Enter a No. '))
+# for i in range (user_input,user_input1+1,1):
+#     print (i,end=",")
+
+# 4 Sigma 
+
+# user_input = int(input('Enter a No. '))
+# a=0
+# for i in range (user_input+1):
+#     print (i,"+ ",end="")
+#     a+=i
+# print("=",a)
+
+#5 Factorial
+
+# user_input = int(input('Enter a No. '))
+# a=1
+# for i in range (1,user_input+1):
+#     print (i,"+ ",end="")
+#     a*=i
+# print("=",a)
+
+# 6 Even / Odd 
+
+# user_input = int(input('Enter a No. '))
+
+# print("Even:- ")
+# for i in range (1,user_input+1):
+#     if(i % 2 == 0):
+#         print (i,end=",")
+
+# print ()
+# print("Odd:- ")
+# for i in range (1,user_input+1):
+#     if(i % 2 != 0):
+#         print (i,end=",")
+    
+# 7 Even / Odd 
+
+# user_input = int(input('Enter a No. '))
+# print("Odd  Even  ")
+# for i in range (1,user_input+1):
+#     if(i % 2 != 0):
+#         print (i,end="\t")
+#     else:
+#         print (i,end="")
+#         print()
+
+
+# 3 & 7   (21, 42, 84)
+
+# user_input = int(input('Enter a No. '))
+
+# print("3 & 7 =")
+# for i in range (1,user_input+1):
+#     if(i % 3 == 0 and i % 7==0):
+#         print (i,end=",")
+
+# 3 & 7   (21, 42, 84)
+
+# user_input = int(input('Enter a No. '))
+
+# print("3 & 7 =")
+# for i in range (1,user_input+1):
+#     if(i % 3 == 0 or i % 7==0):
+#         print (i,end=",")
+
+# 10 Gadiyo
+
+# user_input = int(input('Enter a No. '))
+# for i in range (1,11):
+#         print (f"{user_input} X {i} = {i*user_input}" ,end="\n")
+
+
+# 11 Multi Multiplication Table
+
+user_input = int(input('Enter a No. '))
+user_input2 = int(input('Enter a No. '))
+
+while user_input <= user_input2:
+    for i in range (1,11):
+        print (f"{user_input} X {i} = {i*user_input}" ,end="\n")
+    user_input+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  while loop
+# for loop 
+# array 
+# Pattern  paraller 
+#  
