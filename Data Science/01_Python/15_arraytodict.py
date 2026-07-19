6ddf8e732ab9c0f9775714c6eb8413ed31e96665
Yug Patel <pyugpatel189@gmail.com>
2026-07-19 11:35:01 +0530

    add more extar files

--- Data Science/01_Python/15_arraytodict.py
+++ Data Science/01_Python/15_arraytodict.py
@@ -65,3 +65,104 @@
 #             dict_no[j] = temp
 
 # print(dict_no)
+
+# 7. Descending Order
+# dict_no = {1:102,2:2,3:12,4:34,5:43,6:78}
+# temp = dict_no[1]
+# for i in range(1,len(dict_no)+1):
+#     for j in range(1,len(dict_no)+1):
+#         if (dict_no[i] <  dict_no[j]):
+#             temp = dict_no[i]
+#             dict_no[i] = dict_no[j]
+#             dict_no[j] = temp
+
+# print(dict_no)
+
+# 8. Second Largest No From Given Array
+# dict_no = {1:10,2:4,3:7,4:2,5:1,6:435,7:9,8:45}
+# largest = dict_no[1]
+
+# for i in range(1,len(dict_no)+1):
+#     if (dict_no[i] > largest):
+#         largest = dict_no[i]
+
+# second_largest= -1
+# for j in range(1,len(dict_no)+1):
+#     if (dict_no[j] > second_largest and dict_no[j] != largest):
+#         second_largest = dict_no[j]
+
+# print(second_largest)
+
+
+# 8. Suppress Zero
+# dict_no = {1:10,2:4,3:0,4:0,5:1,6:435,7:9,8:0}
+# temp = 0
+
+# for j in range (1,len(dict_no)):
+#     for i in range(1,len(dict_no)):
+#         if (dict_no[i] == 0 ):
+#             temp = dict_no[i]
+#             dict_no[i] = dict_no[i+1]
+#             dict_no[i+1]= temp
+
+# print(dict_no)
+
+# 8. Suppress Negative
+# dict_no = {1:10,2:-4,3:0,4:0,5:1,6:-435,7:-9,8:0}
+# temp = 0
+
+# for j in range (1,len(dict_no)):
+#     for i in range(1,len(dict_no)):
+#         if (dict_no[i] < 0 ):
+#             temp = dict_no[i]
+#             dict_no[i] = dict_no[i+1]
+#             dict_no[i+1]= temp
+
+# print(dict_no)
+
+# 8. Suppress Postive
+# dict_no = {1:-10,2:-4,3:0,4:0,5:1,6:-435,7:9,8:0}
+# temp = 0
+
+# for j in range (1,len(dict_no)):
+#     for i in range(1,len(dict_no)):
+#         if (dict_no[i] > 0 ):
+#             temp = dict_no[i]
+#             dict_no[i] = dict_no[i+1]
+#             dict_no[i+1]= temp
+
+# print(dict_no)
+
+
+# 12. Insert Value at Given Position
+
+# dict_no = {1:10,2:20,3:30,4:40,5:50}
+# value =int(input('Enter a value :'))
+# pos =int(input('Enter a Position of New Value: '))
+
+# for i in range(1,len(dict_no)+1):
+#     if(i == pos):
+#         dict_no[i] = value
+
+# print(dict_no)
+
+
+# 13. Delete a Duplicate Values
+
+# dict_no = {1:10,2:20,3:30,4:10,5:20}
+# unique_values = list(set(dict_no.values()))
+# new_dict = dict()
+# for i in range(len(unique_values)):
+#     new_dict[i] = unique_values[i]
+# print(new_dict)
+
+
+# 14. Delete Given Values
+# dict_no = {1:10,2:20,3:30,4:10,5:20}
+# user_input = int(input('Enter a value that you have to delete: '))
+
+# for i in range(1,len(dict_no)+1):
+#     if (dict_no[i] == user_input):
+#         dict_no.pop(i)
+# print(dict_no)
+
