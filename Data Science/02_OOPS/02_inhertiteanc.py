64c90ca89dc4943c7ab7e0692bc95e5b8eb73b61
Yug Patel <pyugpatel189@gmail.com>
2026-08-15 16:28:38 +0530

    add few more files

--- Data Science/02_OOPS/02_inhertiteanc.py
+++ Data Science/02_OOPS/02_inhertiteanc.py
@@ -27,25 +27,73 @@
 # s.show_data()
 
 
-# Pr 1 Simple inheritance
+# Pr 3 
+# class person:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age 
+#     def show_data_person(self):
+#         print("Name: ",self.name)
+#         print("age: ",self.age)
+# class student(person):
+#     def __init__(self,name,age,per):
+#         super().__init__(name,age)
+#         self.per = per
+#     def show_data_student(self):
+#         print("Student")
+#         super().show_data_person()
+#         print("Per: ",self.per)
 
-class Base:
-    def __init__(self, x=0):
-        self.x = x
-    def puls(self):
-        self.x += 1
-    def show_data(self):
-        print("Value of x:", self.x)
+# class teacher(person):
+#     def __init__(self,name,age,salary):
+#             super().__init__(name,age)
+#             self.salary = salary
+#     def show_data_teacher(self):
+#         print("Teacher")
+#         super().show_data_person()
+#         print("Salary: ",self.salary)
+        
 
-class Derived(Base):
-    def __init__(self,x):
-        super().__init__(x)
-    def show_data1(self):
-        x=x-1
-        super().show_data()
-        print("Value of x:", self.x)
+# s = student("Yug",18,80.99)
+# t= teacher("Mr.Sumit",45,100000)
+# s.show_data_student()
+# t.show_data_teacher()
 
-d = Derived(7)
-d.puls()
-d.show_data()
-d.show_data1()
+# Pr 4 tho public & private no che 
+
+# Pr 7 
+# class crickter:
+#     def __init__(self,name,total_matches):
+#         self.name = name
+#         self.total_matches = ""
+#     def read(self):
+#         self.name = input("Enter your name: ")
+#         self.total_matches = int(input('Enter your total matches: '))
+    
+# class batsman(crickter):
+#     def __init__(self,name,total_matches,total_runs,avg_runs,best_performance):
+#         super().__init__(name,total_matches)
+#         self.total_runs = total_runs
+#         self.avg_runs = avg_runs
+#         self.best_performance =best_performance
+
+#     def calculate(self):
+#         self.avg_runs = self.total_runs/self.total_matches
+#     def show(self):
+#         self.calculate()
+#         print("Total Matches",self.total_matches)
+#         print("Total Runs",self.total_runs)
+#         print("Avg",self.avg_runs)
+#         print("Best performance",self.best_performance)
+
+# b = batsman("",0,9876,0,189)
+# b.read()
+# print("You have entered")
+# b.show()
+
+
+
+         
+
+
+
